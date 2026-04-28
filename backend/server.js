945bef89ff2e51cafd78eab5f41b43339995deb0
Sakshi <sakshiparashar2910@gmail.com>
2026-04-28 10:07:16 +0530

    feat(frontend): add the backend deploy url link to frontend env file

--- backend/server.js
+++ backend/server.js
@@ -15,6 +15,7 @@ app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 
+// https://deknek3d-assignment.onrender.com/
 app.listen(PORT, () => {
     console.log("Server is running...");
 });
